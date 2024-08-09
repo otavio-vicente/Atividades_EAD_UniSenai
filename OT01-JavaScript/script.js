bcb5b73062853164ec8c124208bda75715a10f74
Otávio Henrique Vicente <101840512+otavio-vicente@users.noreply.github.com>
2024-08-09 20:03:35 -0300

    Finalização OT01-JavaScript

---
+++ OT01-JavaScript/script.js
@@ -0,0 +1,9 @@
+let nome = "Otávio Henrique Vicente";
+let idade = 19;
+let comidaFavorita = "pizza";
+
+let msg = "Olá, meu nome é " + nome + ", tenho " + idade + " anos de idade e minha comida favorita é " + comidaFavorita + ".";
+
+console.log("Olá, meu nome é " + nome + ", tenho " + idade + " anos de idade e minha comida favorita é " + comidaFavorita + ".");
+
+alert(msg);
