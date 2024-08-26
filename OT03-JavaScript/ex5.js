6b947d4272eba815718caff080ec35e95fcc0410
Otávio Henrique Vicente <101840512+otavio-vicente@users.noreply.github.com>
2024-08-26 11:29:01 -0300

    OT03 - JavaScript

---
+++ OT03-JavaScript/ex5.js
@@ -0,0 +1,6 @@
+for(let i=0; i<=20; i++){
+    if(i == 13){
+        continue;
+    }
+    console.log("Número: " + i);
+}
---
+++ OT03-JavaScript/ex6.js
@@ -0,0 +1,7 @@
+let nomes = ['Luquinhas', 'Rafa', 'Indio'];
+let i=1;
+
+for(let nome of nomes){
+    console.log("Nome "+ (i) + ": " + nome);
+    i++;
+}
